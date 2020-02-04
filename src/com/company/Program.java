336a73aabbe3488a68555ecec062b90c1c814203
doha6991 <dogge20@hotmail.com>
2020-02-04 16:09:11 +0100

    main method

--- src/com/company/Program.java
+++ src/com/company/Program.java
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit;
 
 public class Program {
 	// Static variables.
-	private static final int NUM_THREADS = 1;
+	private static final int NUM_THREADS = 4;
 	private static final int NUM_ACCOUNTS = 300;
 	private static final int FACTOR = 10000;
 	private static final int TIMEOUT = 60; // Seconds;
